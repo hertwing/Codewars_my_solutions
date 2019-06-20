f623b95ed8eb3dc6a5c90e1c823bab9611e9b9bc
hertwing <henczel.piotr@gmail.com>
2019-06-20 08:00:30 +0200

    Create bouncing_balls.cpp

---
+++ C++/bouncing_balls.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+class Bouncingball
+{
+public:
+    static int bouncingBall(double h, double bounce, double window)
+    {
+      if(h<0 || bounce <= 0 || bounce >=1 || window >=h)
+        return -1;
+      else {
+        int count = 1;
+        h*=bounce;
+        while(h>=window)
+        {
+          count+=2;
+          h*=bounce;
+        }
+        return count;
+      }
+    }
+};
